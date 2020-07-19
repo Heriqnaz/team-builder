fdedda2ca4363f7c50fac196693c290e59a86f96
developer <heriqnazsim@gmail.com>
2020-07-19 13:06:11 +0400

    build

---
+++ build/precache-manifest.2530d8d4e796dccb7fe1b8dfb3502ae6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7454edd99577a344bc7135744e4c565",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fb324271f5502241427",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "18e3d9e1a93350048da4",
+    "url": "/static/css/main.9fec4a18.chunk.css"
+  },
+  {
+    "revision": "8fb324271f5502241427",
+    "url": "/static/js/2.37020982.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.37020982.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18e3d9e1a93350048da4",
+    "url": "/static/js/main.ec351553.chunk.js"
+  },
+  {
+    "revision": "8b8f8712f7998908b0a9",
+    "url": "/static/js/runtime-main.8c44053c.js"
+  }
+]);
